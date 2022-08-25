eba90732925511e96e5bd787078ac4850bd11bf7
kot613 <kot613@gmail.com>
2022-08-26 00:38:21 +0300

    Chapter 14: A Better Application Structure, add .env

--- config.py
+++ config.py
@@ -1,19 +1,23 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
     # SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'you-will-never-guess')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'app.db'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     POSTS_PER_PAGE = 5
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = 1
-    MAIL_USERNAME = 'kot613@gmail.com'
-    MAIL_PASSWORD = 'jafptctijdjfbpke'
-    ADMINS = ['kot613@gmail.com', 'mashulik851@fmail.com']
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['kot613@gmail.com', ]
 
     LANGUAGES = ['en', 'uk']
